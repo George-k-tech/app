4eaa08dac255a3d496802bea6203577c5b935c37
George-k-tech <kariukigeorge511@gmail.com>
2023-06-20 13:48:47 +0300

    changes on products layout in shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -11,8 +11,6 @@ class ShopController extends Controller
     public function index():View
     {
         $products = Product::all();
-        $products=Product::orderBy('id')->paginate(1);
-
         return view('shop.index', compact('products'));
     }
 }
