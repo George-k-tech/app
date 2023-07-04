d5e5b3bf0eebd16045d45ebc2097b62b488db363
George-k-tech <kariukigeorge511@gmail.com>
2023-07-04 20:30:18 +0300

    css

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,7 +5,8 @@
         </h2>
     </x-slot> --}}
 
-    <div class="">
+    <h1 id="hello">hello george</h1>
+   {{--  <div class="">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
             <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
               @foreach ($products as $item)
@@ -34,5 +35,5 @@
             </div>
         </div>
     </div>
-
+ --}}
 </x-app-layout>
