8024933fa8966d20c83ddcbac9af0f88abad16ca
George-k-tech <kariukigeorge511@gmail.com>
2023-07-21 19:27:11 +0300

    model for order, migration for the order

--- database/migrations/2023_07_21_162636_create_orders_table.php
+++ database/migrations/2023_07_21_162636_create_orders_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->integer('productId')->default(0);
-            $table->integer('quantity')->default(0);
-            $table->integer('customerId')->default(0);
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('orders');
     }
 };
