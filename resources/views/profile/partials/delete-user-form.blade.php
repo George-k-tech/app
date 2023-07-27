36ce5e4ebb33c286c5c7c622ae267c4865a52c27
George-k-tech <kariukigeorge511@gmail.com>
2023-07-27 23:44:34 +0300

    updates on the profile page

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,9 +1,17 @@
 <section class="space-y-6">
  
+
+    <div>
+        <h2>Delete Account</h2>
+
+        <p>Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.</p>
+    </div>
+
    <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
     >{{ __('Delete Account') }}</x-danger-button>
+
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,5 +1,10 @@
 <section>
     
+    <div>
+        <h2>update password</h2>
+
+        <p>Ensure your account is using a long, random password to stay secure.</p>
+    </div>
 
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
