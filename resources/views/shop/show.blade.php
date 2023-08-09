07f4df6772e0b56a8f00815b7807bb869d9ad590
George-k-tech <kariukigeorge511@gmail.com>
2023-08-09 15:18:56 +0300

    css

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -1,27 +1,28 @@
 <x-app-layout>
-  
+
     <section class="products section" id="products">
-        <h1 class="heading"><span>{{$category->name}}</span></h1>
+        <h1 class="heading"><span>{{ $category->name }}</span></h1>
         <div class="box-container">
             @foreach ($category->products as $product)
                 <div class="box">
                     <span class="discount">-10%</span>
                     <div class="image">
-                        <img src={{ $product->image }} alt="this product">
-                        <div class="icons">
-                            <a href="#" class="fas fa-heart"></a>
-                            <a href="{{ route('add-to-cart', $product->id) }}" class="cart-btn">Add To Cart</a>
-                            <a href="#" class="fas fa-share"></a>
-                        </div>
+                        <a href="{{ url('/shop/' . $category->slug . '/' . $product->slug) }}"><img src={{ $product->image }}
+                                alt="this product"></a>
                     </div>
                     <div class="content">
                         <h3>{{ $product->name }}</h3>
-                        <div class="price">
-                            ${{ $product->price }} <span>$the price was</span></div>
+                        <div class="price"> ${{ $product->price }} <span>$the price was</span></div>
+                        <p class="content-desc">{{ $product->description }}</p> 
+                    </div>
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="{{ route('add-to-cart', $product->id) }}" class="cart-btn">Add To Cart</a>
+                        <a href="#" class="fas fa-share"></a>
                     </div>
                 </div>
             @endforeach
-        
+
         </div>
     </section>
 
